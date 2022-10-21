0358fa95f3b5aaeaf7d7fd2f87ba715ce5247d2c
setare <setarehsaboory@gmail.com>
2022-10-21 14:29:09 +0200

    validate

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 let alphabet=document.querySelectorAll(".letter");
-let word="HYPER";
+const Scene=document.getElementById('game-container');
+let word=prompt('Enter the word to guess: ').toUpperCase();
+console.log(word);
 let letters=word.split("");
-
+let lives=0;
+//who comes firrst below?
+SetScene(letters);
+function SetScene(letters) {
+    for (let i = 0; i < letters.length; i++) {
+        Scene.innerHTML+=" _ ";
+    }
+}
+function SetLetter(letter,index) {
+    
+}
 function ValidateLetter() {
     const ClickedLetter=event.target.innerHTML;
     let used=false;
@@ -9,20 +21,28 @@ function ValidateLetter() {
         if (ClickedLetter===letters[i]) {
             console.log(letters[i]+' ok');
             used=true;
+            let index=i;
+            SetLetter(ClickedLetter,i);
         }
     }
-    KeyboardUpdate(used,event.target);
+    KeyboardUpdate(used,event.target,);
 }
 
 function KeyboardUpdate(key,button) {
+    button.disabled=true;
+    button.style.color='white';
     if (key===true) {
         console.log("it was used");
         button.style.backgroundColor='rgb(255, 118, 118)';
-        console.log(button);
+        updateScene(button);
     }
     else{
         console.log("it was not used");
         button.style.backgroundColor='rgb(146, 124, 124)';
+        lives++;
+        console.log(lives);
     }
 }
-
+function updateScene(button) {
+    Scene.innerHTML=(button.innerHTML)
+}
