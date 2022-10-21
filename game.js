68dec91a5dbc21acf5a4d4cd8d81b30dea230d49
setare <setarehsaboory@gmail.com>
2022-10-22 00:20:17 +0200

    update true scene

--- game.js
+++ game.js
@@ -4,43 +4,41 @@ let word=prompt('Enter the word to guess: ').toUpperCase();
 console.log(word);
 let letters=word.split("");
 let lives=0;
+
 //who comes firrst below?
-SetScene(letters);
+
 function SetScene(letters) {
     for (let i = 0; i < letters.length; i++) {
-        let spot=document.createElement('span').innerHTML=" _ ";
+        let spot=document.createElement('span');
+        spot.classList.add('spot');
+        spot.innerHTML=" _ ";
         Scene.append(spot);
     }
 }
+SetScene(letters);
+
 function UpdateScene(letter,index) {
-    for (let i = 0; i < letters.length; i++) {
-        if (i===index) {
-            Scene.innerHTML+=letter;
-            console.log(Scene);
-        }
-        else {
-            Scene.innerHTML+="_";
-        }
-    }
+    let CurrentScene=document.querySelectorAll('.spot');
+    console.dir(CurrentScene[0].innerHTML+"   ddddd");
+    CurrentScene[index].innerHTML=letter;
 }
+
 function ValidateLetter() {
     const ClickedLetter=event.target.innerHTML;
     let used=false;
     for (let i = 0; i < letters.length; i++) {
         if (ClickedLetter===letters[i]) {
-            console.log(letters[i]+' is used');
             used=true;
             UpdateScene(letters[i],i);
         }
     }
-    KeyboardUpdate(used,event.target,);
+    KeyboardUpdate(used,event.target);
 }
 
 function KeyboardUpdate(key,button) {
     button.disabled=true;
     button.style.color='white';
     if (key===true) {
-        console.log("it was used");
         button.style.backgroundColor='rgb(255, 118, 118)';
         updateScene(button);
     }
@@ -50,7 +48,4 @@ function KeyboardUpdate(key,button) {
         lives++;
         console.log(lives);
     }
-}
-function updateScene(button) {
-    Scene.innerHTML=(button.innerHTML)
 }
