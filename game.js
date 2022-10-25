d1ea0ed0722b2208823babe0fb086f9a5f998bc3
setare <setarehsaboory@gmail.com>
2022-10-25 17:11:15 +0200

    keyboard updated

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 'use strict'
 const form=document.querySelector('#input-form');
-
 const alphabet=document.querySelectorAll('.letter');
 const Scene=document.getElementById('game-container');
 const livesDisplay=document.getElementById('display-life');
@@ -8,7 +7,139 @@ const againContainer=document.getElementById('again-btn');
 let CurrentSpots=[];
 let letters=[];
 let lives=0;
+let guessingWord='';
 let clickedLetter='empty';
+
+form.onsubmit=function(event) {
+    event.preventDefault();
+    guessingWord=form.elements.wordToGuess.value;
+    if (!guessingWord) {
+        alert("there is no word to guess !")
+    }
+    if (guessingWord) {
+        letters=guessingWord.toUpperCase().split("");
+        form.elements.wordToGuess.value='';
+        SetScene(letters); 
+    }
+}
+alphabet.forEach(element => {
+    element.addEventListener('click', function(event) {
+        event.stopPropagation();
+        playTurn(element);
+        
+    })
+});
+
+function playTurn(btn) {
+    clickedLetter=btn.innerHTML;
+    if (validate(clickedLetter)) {
+        console.log(clickedLetter+" letter is used");
+        updateKeyboard(btn,true);
+        win(CurrentSpots);
+    }
+    else{
+        console.log(clickedLetter+" letter is not used");
+        updateKeyboard(btn,false);
+        lose();
+    }
+}
+
+function validate(clickedLetter) {
+    let used=false
+    for (let i = 0; i < letters.length; i++) {
+        if (clickedLetter===letters[i]) {
+            UpdateScene(letters[i],i);
+            used=true;
+        } 
+    }
+   return used;
+}
+// function win(CurrentSpots) {
+        
+// }
+function SetScene(letters) {
+    Scene.innerHTML="";
+    for (let i = 0; i < letters.length; i++) {
+        let spot=document.createElement('span')
+        spot.classList.add('spot');
+        spot.innerHTML=" _ ";
+        Scene.append(spot);
+        //lifes can be aaded here in update visual
+    }
+    // livesDisplay.innerHTML="you have 5 chances";
+    lives=5;
+    // displayLive(lives);
+}
+function lose() {
+    lives--;
+    if (lives<=0) {
+        alert("GAME OVER ! it was "+ guessingWord);
+        disableKeys();
+    }
+}
+function win(spots) {
+    let check=[];
+    for (let i = 0; i < spots.length; i++) {
+        check[i]=spots[i].innerHTML;
+    }
+    if (!check.includes(" _ ")) {
+        alert("YES IT IS "+letters.join('')+" YOU WON!");
+        disableKeys();
+    }
+ }
+
+function UpdateScene(letter,index) {
+    CurrentSpots=document.querySelectorAll('.spot');
+    CurrentSpots[index].innerHTML=letter;
+}
+
+function disableKeys() {
+    for (let i = 0; i < alphabet.length; i++) {
+        alphabet[i].disabled=true;
+    }
+}
+
+// function displayLive(lives) {
+//     for (let i = 0; i < lives; i--) {
+//         let img=document.createElement("img");
+//         img.src="heart.png";
+//         livesDisplay.append(img);
+//     }
+
+// }
+
+
+function updateKeyboard(button,state) {
+    button.disabled=true;
+    button.style.color='white';
+    //add extra css later
+    if (state===true) {
+        button.style.backgroundColor='rgb(255, 118, 118)';
+    }
+    else{
+        button.style.backgroundColor='rgb(146, 124, 124)';
+    }
+}
+
+
+
+
+// function enableleKeys() {
+//     for (let i = 0; i < alphabet.length; i++) {
+//         alphabet[i].disabled=false;
+//         alphabet[i].style.backgroundColor='';
+//         alphabet[i].style.color='';
+//     }
+// }
+// function playAgain() {
+//     let toPlay=confirm("DO YOU WANT TO PLAY AGAIN ?");
+//     if (toPlay) {
+//         Scene.innerHTML="";
+//         enableleKeys();
+//     }
+// }
+
+
 
 // function playTurn() {
 //     const letter = event.target.innerHTML;
@@ -30,134 +161,4 @@ let clickedLetter='empty';
 //     }
 
 
-// }
-
-form.onsubmit=function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    let guessingWord=form.elements.wordToGuess.value;
-    if (!guessingWord) {
-        alert("there is no word to guess !")
-    }
-    if (guessingWord) {
-        letters=guessingWord.toUpperCase().split("");
-        form.elements.wordToGuess.value='';
-        SetScene(letters); 
-    }
-}
-alphabet.forEach(element => {
-    element.addEventListener('click', function(event) {
-        event.stopPropagation();
-        clickedLetter=element.innerHTML;
-        playTurn(clickedLetter);
-    })
-});
-
-function playTurn(clickedLetter) {
-    if (validate(clickedLetter)) {
-        console.log(clickedLetter+" letter is used");
-        win(CurrentSpots);
-    }
-    else{
-        console.log(clickedLetter+"  it does not exist");
-        // losingCheck();
-    }
-}
-
-function validate(clickedLetter) {
-    let used=false
-    for (let i = 0; i < letters.length; i++) {
-        if (clickedLetter===letters[i]) {
-            UpdateScene(letters[i],i);
-            used=true;//bot used yet
-            return true;
-        }
-
-    }
-    // KeyboardUpdate(used,clickedLetter);
-    
-}
-// function win(CurrentSpots) {
-        
-// }
-function SetScene(letters) {
-    Scene.innerHTML="";
-    for (let i = 0; i < letters.length; i++) {
-        let spot=document.createElement('span')
-        spot.classList.add('spot');
-        spot.innerHTML=" _ ";
-        Scene.append(spot);
-        //lifes can be aaded here in update visual
-    }
-    // livesDisplay.innerHTML="you have 5 chances";
-    lives=5;
-    // displayLive(lives);
-}
-
-function win(spots) {
-    let check=[];
-    for (let i = 0; i < spots.length; i++) {
-        check[i]=spots[i].innerHTML;
-    }
-    if (!check.includes(" _ ")) {
-        alert("YES IT IS "+letters.join('')+" YOU WON!");
-        disableKeys();
-    }
- }
-
-function UpdateScene(letter,index) {
-    CurrentSpots=document.querySelectorAll('.spot');
-    CurrentSpots[index].innerHTML=letter;
-}
-
-
-// function displayLive(lives) {
-//     for (let i = 0; i < lives; i--) {
-//         let img=document.createElement("img");
-//         img.src="heart.png";
-//         livesDisplay.append(img);
-//     }
-
-// }
-
-
-// function KeyboardUpdate(state,button) {
-//     button.disabled=true;
-//     button.style.color='white';
-//     if (state===true) {
-//         console.log("it was used");
-//         button.style.backgroundColor='rgb(255, 118, 118)';
-//     }
-//     else{
-//         console.log("it was not used");
-//         button.style.backgroundColor='rgb(146, 124, 124)';
-//         lives--;
-//     }
-// }
-// function losingCheck() {
-//     if (lives<=0) {
-//         alert("GAME OVER ! it was "+word);
-//         disableKeys();
-//         playAgain();
-//     }
-//     displayLive(lives);
-// }
-function disableKeys() {
-    for (let i = 0; i < alphabet.length; i++) {
-        alphabet[i].disabled=true;
-    }
-}
-// function enableleKeys() {
-//     for (let i = 0; i < alphabet.length; i++) {
-//         alphabet[i].disabled=false;
-//         alphabet[i].style.backgroundColor='';
-//         alphabet[i].style.color='';
-//     }
-// }
-// function playAgain() {
-//     let toPlay=confirm("DO YOU WANT TO PLAY AGAIN ?");
-//     if (toPlay) {
-//         Scene.innerHTML="";
-//         enableleKeys();
-//     }
 // }
