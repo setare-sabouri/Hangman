c2c39311f859541dea52709efebbb878bd3a2e21
setareh <setarehsaboory@gmail.com>
2025-09-07 03:36:25 +0200

    new model

--- src/Components/LandingPage/HangedMan.jsx
+++ src/Components/LandingPage/HangedMan.jsx
@@ -1,39 +1,35 @@
-import React from 'react'
-import { Center, Float, useGLTF,Text } from '@react-three/drei'
+import { Center, useGLTF, Text, Text3D } from '@react-three/drei'
 
 const HangedMan = () => {
-    const HangedMan = useGLTF('/hanged_man.glb')
+  const HangedMan = useGLTF('/hangman.glb')
   return (
-     <Center>
-          <Float  floatIntensity={1}  >
-          <primitive object={HangedMan.scene} scale={4} />
-          
-          <Text 
-            font="./bebas-neue-v9-latin-regular.woff"
-            fontSize={2}
-             color="white" 
-             position={[-6, 5, 0]} 
-             anchorX="center"
-             anchorY="middle"
-            
-            onClick={() => window.location.href = '/game/onePlayer'}
-            >
-                Single Player
-            </Text>
-            <Text 
-            font="./bebas-neue-v9-latin-regular.woff"
-            fontSize={2}
-             color="white" 
-             position={[-6, 2, 0]} 
-             anchorX="center"
-              anchorY="middle"
-              
-              onClick={() => window.location.href = '/game/twoPlayer'}
-              >
-                Two Player
-            </Text>
-        </Float>
-    
+    <Center>
+        <primitive position={[0,-0.5,0]} object={HangedMan.scene} scale={0.9} />
+        <Text
+          font="./bebas-neue-v9-latin-regular.woff"
+          fontSize={2}
+          color="white"
+          position={[-6, 5, 0]}
+          anchorX="center"
+          anchorY="middle"
+
+          onClick={() => window.location.href = '/game/onePlayer'}
+        >
+          Single Player
+        </Text>
+        <Text
+          font="./bebas-neue-v9-latin-regular.woff"
+          fontSize={2}
+          color="white"
+          position={[-6, 2, 0]}
+          anchorX="center"
+          anchorY="middle"
+
+          onClick={() => window.location.href = '/game/twoPlayer'}
+        >
+          Two Player
+        </Text>
+
     </Center>
   )
 }
